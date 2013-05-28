03540f2a412d4fa9d8c7a2e07b61818673ecb677
unknown <starfish internet@.(none)>
2013-05-28 16:48:56 +0800

    slider changes

--- Project/Design/1_Website/Pages/primary/home/templates/inpage_javascript.js
+++ Project/Design/1_Website/Pages/primary/home/templates/inpage_javascript.js
@@ -1,15 +1,33 @@
 $(document).ready(function() {
-	setInterval("rotateImages()", 8000);
+	$('.sliderNavigation').click(function(){
+		if($(this).attr('id') == 'next')
+			getNextImage();
+		else
+			getPrevImage();	
+	});
+	setInterval("getNextImage()", 8000);
 });
 
-function rotateImages() {
-    var CurPhoto = $('#slider div.active');
+function getNextImage() {
+    var CurPhoto = $('#slider > div.active');
     var NxtPhoto = CurPhoto.next();
     if (NxtPhoto.length == 0)
-        NxtPhoto = $('#slider div:first');
+        NxtPhoto = $('#slider > div:first');
 
     CurPhoto.removeClass('active').addClass('previous');
     NxtPhoto.css({ opacity: 0.0 }).addClass('active').animate({ opacity: 1.0 }, 1000,
+        function() {
+            CurPhoto.removeClass('previous');
+        });
+	}
+function getPrevImage() {
+    var CurPhoto = $('#slider > div.active');
+    var PrevPhoto = CurPhoto.prev();
+    if (PrevPhoto.length == 0)
+    	PrevPhoto = $('#slider > div:last');
+
+    CurPhoto.removeClass('active').addClass('previous');
+    PrevPhoto.css({ opacity: 0.0 }).addClass('active').animate({ opacity: 1.0 }, 1000,
         function() {
             CurPhoto.removeClass('previous');
         });
